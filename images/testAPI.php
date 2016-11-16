4c60090402f64937547c41fdcccd54ecb67ca9e6
Holocentrus <Holocentrus@users.noreply.github.com>
2016-11-16 15:22:46 -0500

    added test API
    attached

---
+++ images/testAPI.php
@@ -0,0 +1,14 @@
+{
+  "name": "World Register of Marine Species",
+  "identifierSpace": "urn:lsid:marinespecies.org:taxname:",
+  "schemaSpace": "http:\/\/rdf.freebase.com\/ns\/type.object.id",
+  "view": {
+    "url": "http:\/\/www.marinespecies.org\/aphia.php?p=taxdetails&id={{id}}"
+},
+"defaultTypes": [
+  {
+    "id": "\/biology\/organism_classification\/scientific_name",
+    "name": "Scientific name"
+    }
+  ]
+}
